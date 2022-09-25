42e676f5c3aa419ad469a07386e541e052992b0c
jd <jdanielxsilva@gmail.com>
2022-09-25 17:01:13 -0300

    alterações de domingo

--- DESAFIO.py
+++
@@ -1,7 +0,0 @@
-n=int(input('uma nota entre zero e 10: '))
-while n > 10:
-    print('errado!')
-while n < 0:
-    print('errado!')
-if n == 
-print('correto')
---
+++ TESTES.py
@@ -0,0 +1,4 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+print(texto.find("a"))
+print(texto.find("A"))
+print(texto.find("ã"))
