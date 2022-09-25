bcd94258504f920d66abfc9980b890dbaefae327
jd <jdanielxsilva@gmail.com>
2022-09-25 19:08:34 -0300

    domingo

--- TESTES.py
+++ TESTES.py
@@ -1,4 +1,1 @@
-texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
-print(texto.find("a"))
-print(texto.find("A"))
-print(texto.find("ã"))
+a = input("digite 4 numeros : ")
