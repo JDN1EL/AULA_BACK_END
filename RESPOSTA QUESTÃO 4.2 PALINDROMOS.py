c6154762ae1a319bf5ab35b11ad5d9805208cef7
jd <jdanielxsilva@gmail.com>
2022-10-05 18:57:33 -0300

    dia de desafio

---
+++ RESPOSTA QUESTÃO 4.2 PALINDROMOS.py
@@ -0,0 +1,6 @@
+#04 - Faça um programa que recebe uma string:
+#• imprime “Palíndromo” caso a string seja um palíndromo
+#• imprime “Não é palíndromo” caso não seja
+#• Palíndromo é uma palavra ou frase, que é igual quando lida da esquerda para a direita ou da direita para a esquerda (espaços em brancos são descartados). 
+#• Exemplos de palíndromo: “ovo”, “reviver”, “mega bobagem”, 
+#“anotaram a data da maratona”
