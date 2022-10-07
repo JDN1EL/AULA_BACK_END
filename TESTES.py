52b46866d9c967f6e8b316ea5ecf3316f703b078
jd <jdanielxsilva@gmail.com>
2022-10-07 18:33:08 -0300

    07/10

--- TESTES.py
+++ TESTES.py
@@ -35,4 +35,21 @@ funções referentes ao banco
 nota: funções que vão fazer o que há nos outros 
 
 identificação de arquivos"""
+#import random
+#n = 1
+#a = 9
+#lista = [a*random.random() for n in range(n)]
+#print(lista)#                       ______
+    #                                    |__________range cria uma sequencia de numeros 
+#                                                
 
+cc = ""
+if cc == "":
+        print('Esse é o seu número de cliente\nLembrese de anotar e guardá-lo')
+        import random
+        total_de_listas = 1
+        caractere_extra = 9
+        numero_cliente = [caractere_extra*random.random() for total_de_listas in range(total_de_listas)]
+        print (numero_cliente[::-1])
+        nca = {'ncarmazenado':numero_cliente}
+        print (nca ['ncarmazenado'])
---
+++ pj cash/pj_transações.py
@@ -0,0 +1,16 @@
+from ast import Str
+
+
+um = 1
+dois = 2
+inicio = int(input('Olá! bem vindo(a)!\nPara seu cadastro pressione 1 se não, pressione 2\n'))
+from cliente import dados, submenu
+if inicio == um:
+    while True:
+        dados
+        if submenu == 1:
+            from conta_corrente import coleta_de_dados_um
+        
+        
+if inicio == dois:
+    exit
